a30d067f40896d7d3e82a68fd05de2bc792dd18b
clod2008 <claudio@apsis.com.ar>
2021-11-02 18:03:32 -0300

    maqueta 0.1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
 
 import './styles.css';
 
-import siteBrand from './assets/img/images/2x/GMS-logo@2x.png';
+import siteBrand from './assets/img/images/2x/g-dental@2x.png';
 import { navBar } from './js/navBar';
 import { homeCarousel } from './js/homeSlider';
 import { formHome } from './js/cta1';
